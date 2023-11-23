8ed71499eba35e101a51f9afb49325e67e480383
m4udar <109977003+m4udar@users.noreply.github.com>
2023-11-23 09:24:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var HTTP_PORT = 8000
+var HTTP_PORT = 8080
 
 // Start server
 app.listen(HTTP_PORT, () => {
